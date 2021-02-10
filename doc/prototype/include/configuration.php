af698bcc3605911e8c0224cdf000d0e516ac1aed
Hy-Vong <blobivon@yahoo.fr>
2021-02-10 10:46:04 -0500

    reagencement des captures prototypes

--- doc/prototype/include/configuration.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]."/dao/");
-define("CHEMIN_INCLUDE", $_SERVER["DOCUMENT_ROOT"]."/include/");
--- doc/prototype/include/pied-page.php
+++
@@ -1,8 +0,0 @@
-        <footer>
-            <div>
-                <span>&copy; Guillaume Vernaz , Maxime Daigle Vigneault , Hy-vong Georges dit Rap, Esteban Escurdero</span>
-            </div>
-        </footer>
-
-    </body>
-</html>
