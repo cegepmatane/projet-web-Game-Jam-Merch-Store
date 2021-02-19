f23693957276851ebf98e35408f2af011e5b4701
EstEsc282 <estebanescudero282@gmail.com>
2021-02-19 01:22:45 -0500

    Update #13

--- admin/supprimer-item.php
+++ admin/supprimer-item.php
@@ -1,7 +1,7 @@
 <?php
 
-define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]."/projet-web/dao/");
-define("CHEMIN_INCLUDE", $_SERVER["DOCUMENT_ROOT"]."/projet-web/include/");
+define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]."/dao - Raccourci/");
+define("CHEMIN_INCLUDE", $_SERVER["DOCUMENT_ROOT"]."/include - Raccourci/");
 
 require_once CHEMIN_ACCESSEUR."PromotemyjamDAO.php";
 require_once CHEMIN_INCLUDE."entete.php";
