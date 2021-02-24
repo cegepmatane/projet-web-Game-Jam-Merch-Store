449c1e17866e1d86e52bdb080d4350d00fd013a2
Hy-Vong <blobivon@yahoo.fr>
2021-02-23 23:53:40 -0500

    Ajout du session_start

--- include/configuration.php
+++ include/configuration.php
@@ -3,3 +3,4 @@ define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]."/dao/");
 define("CHEMIN_MODELE", $_SERVER["DOCUMENT_ROOT"]."/modele/");
 define("CHEMIN_IMAGE", $_SERVER["DOCUMENT_ROOT"]."/img/");
 define("CHEMIN_INCLUDE", $_SERVER["DOCUMENT_ROOT"]."/include/");
+session_start();
