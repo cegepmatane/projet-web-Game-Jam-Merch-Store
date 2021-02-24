77de68d1f50208a1dfeeb3f996fd1a6bb22086f0
EstEsc282 <estebanescudero282@gmail.com>
2021-02-24 00:15:42 -0500

    Edition Profil #21

--- profil.php
+++ profil.php
@@ -6,8 +6,8 @@ require_once CHEMIN_INCLUDE."entete.php";
  
 mysql_select_db("gestioncv") or die('Impossible de selectionner une base de donnee. Assurez vous d\'avoir correctement remplit les donneess de 
 connections.');
-    if (isset($_GET['idmembre'])){
-	$id = (int)$_GET['id'];
+    if (isset($_POST['id'])){
+	$id = (int)$_POST['id'];
 	$result mysql_query("SELECT * FROM membre WHERE id = $id");
 	$affiche_data  = mysql_fetch_assoc($result);
     }
