ec1420b72c0e196bd94c8be866603d67facc19be
EstEsc282 <estebanescudero282@gmail.com>
2021-02-23 22:16:27 -0500

    Edition Profil #21

--- profil.php
+++ profil.php
@@ -5,6 +5,73 @@ include "include/configuration.php";
 require_once CHEMIN_ACCESSEUR."PromotemyjamDAO.php";
 require_once CHEMIN_INCLUDE."entete.php";
 
+if (!isset($_SESSION['id'])){
+        header('Location: index.php');
+        exit;
+    }
+ 
+    // On récupère les informations de l'utilisateur connecté
+    $afficher_profil = $DB->query("SELECT * 
+        FROM membre 
+        WHERE id = ?",
+        array($_SESSION['id']));
+    $afficher_profil = $afficher_profil->fetch();
+ 
+    if(!empty($_POST)){
+        extract($_POST);
+        $valid = true;
+ 
+        if (isset($_POST['modification'])){
+            $nom = htmlentities(trim($nom));
+            $prenom = htmlentities(trim($prenom));
+            $mail = htmlentities(strtolower(trim($mail)));
+ 
+            if(empty($nom)){
+                $valid = false;
+                $er_nom = "Il faut mettre un nom";
+            }
+ 
+            if(empty($prenom)){
+                $valid = false;
+                $er_prenom = "Il faut mettre un prénom";
+            }
+ 
+            if(empty($mail)){
+                $valid = false;
+                $er_mail = "Il faut mettre un mail";
+ 
+            }elseif(!preg_match("/^[a-z0-9\-_.]+@[a-z]+\.[a-z]{2,3}$/i", $mail)){
+                $valid = false;
+                $er_mail = "Le mail n'est pas valide";
+ 
+            }else{
+                $req_mail = $DB->query("SELECT mail 
+                    FROM membre 
+                    WHERE courriel = ?",
+                    array($mail));
+                $req_mail = $req_mail->fetch();
+ 
+                if ($req_mail['mail'] <> "" && $_SESSION['mail'] != $req_mail['mail']){
+                    $valid = false;
+                    $er_mail = "Ce mail existe déjà";
+                }
+            }
+ 
+            if ($valid){
+ 
+                $DB->insert("UPDATE membre SET prenom = ?, nom = ?, courriel = ? 
+                    WHERE id = ?", 
+                    array($prenom, $nom,$mail, $_SESSION['id']));
+ 
+                $_SESSION['nom'] = $nom;
+                $_SESSION['prenom'] = $prenom;
+                $_SESSION['courriel'] = $mail;
+ 
+                header('Location:  profil.php');
+                exit;
+            }   
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -32,13 +99,13 @@ require_once CHEMIN_INCLUDE."entete.php";
             <div id="informations-compte">
                 <label for="nom-utilisateur">
 				<?php
-                if (isset($nom_utilisateur)){
+                if (isset($nom)){
                 ?>
-                    <div><?= $nom_utilisateur ?></div>
+                    <div><?= $nom ?></div>
 				<?php
                 }
                 ?>
-                    Nom d'utilisateur <input type="text" placeholder="Votre nom d'utilisateur id="nom-utilisateur" name="nom-utilisateur" value="<?php if(isset($prenom)){ echo $nom_utilisateur; }else{ echo $afficher_profil['nom_utilisateur'];}?>" required>
+                    Nom d'utilisateur <input type="text" placeholder="Votre nom id="nom" name="nom" value="<?php if(isset($nom)){ echo $nom; }else{ echo $afficher_profil['nom'];}?>" required>
 					
                 </label>
                 <input type="button" value="Changer">
