501a9f6de22e6655a25caf98f0a7951550e0a55c
Hy-Vong Georges Dit Rap <hyvong@mail.freehv.me>
2021-02-23 20:48:17 -0500

    configuration serveur

--- include/configuration.php
+++ include/configuration.php
@@ -1,5 +1,5 @@
 <?php
-define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]."/projet-web/dao/");
-define("CHEMIN_MODELE", $_SERVER["DOCUMENT_ROOT"]."/projet-web/modele/");
-define("CHEMIN_IMAGE", $_SERVER["DOCUMENT_ROOT"]."/projet-web/img/");
-define("CHEMIN_INCLUDE", $_SERVER["DOCUMENT_ROOT"]."/projet-web/include/");
+define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]."/dao/");
+define("CHEMIN_MODELE", $_SERVER["DOCUMENT_ROOT"]."/modele/");
+define("CHEMIN_IMAGE", $_SERVER["DOCUMENT_ROOT"]."/img/");
+define("CHEMIN_INCLUDE", $_SERVER["DOCUMENT_ROOT"]."/include/");
