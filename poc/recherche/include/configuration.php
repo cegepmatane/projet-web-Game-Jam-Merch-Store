d0d58a1778cdb99f83ac97aaddad49ca21932175
EstEsc282 <estebanescudero282@gmail.com>
2021-03-09 23:56:50 -0500

    Création du poc barre de recherche #28

---
+++ poc/recherche/include/configuration.php
@@ -0,0 +1,6 @@
+<?php
+define("CHEMIN_ACCESSEUR", $_SERVER["DOCUMENT_ROOT"]."/poc/recherche/dao/");
+define("CHEMIN_MODELE", $_SERVER["DOCUMENT_ROOT"]."/poc/recherche/modele/");
+define("CHEMIN_IMAGE", $_SERVER["DOCUMENT_ROOT"]."/poc/recherche/img/");
+define("CHEMIN_INCLUDE", $_SERVER["DOCUMENT_ROOT"]."/poc/recherche/include/");
+session_start();
