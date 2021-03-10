77e52f4c2e28790399b076574976d00858b4ff9d
EstEsc282 <estebanescudero282@gmail.com>
2021-03-10 01:23:41 -0500

    Création du poc barre de recherche #28

--- poc/recherche/recherche.js
+++ poc/recherche/recherche.js
@@ -1,6 +1,9 @@
 (function() {
   var httpRequest;
   document.getElementById("recherche").addEventListener('keyup', makeRequest);
+  var url="https://www.promotemyjam.store/poc/recherche/listeItemAjax.php";
+
+
 
   function makeRequest() {
     httpRequest = new XMLHttpRequest();
@@ -10,17 +13,39 @@
       return false;
     }
     httpRequest.onreadystatechange = alertContents;
-    httpRequest.open('GET', 'test.html');
+    httpRequest.onreadystatechange = listeItem;
+    httpRequest.open('GET', url, true);
     httpRequest.send();
   }
 
   function alertContents() {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
+        var response = JSON.parse(httpRequest.responseText);
         alert(httpRequest.responseText);
       } else {
         alert('Il y a eu un problème avec la requête.');
       }
     }
+  }
+
+  function listeItem(){
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        var response = JSON.parse(httpRequest.responseText);
+        alert(httpRequest.responseText);
+      } else {
+        alert('Il y a eu un problème avec la requête.');
+      }
+      echo("1");
+      for (var i = 0; i < response.getElementByTagName('nom').length; i++) {
+      response.getElementByTagName('nom').values('nom');
+      echo("2");
+    }
+    }
+  }
+
+  function afficherItemRechercher(){
+
   }
 })();
