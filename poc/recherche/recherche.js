e0292774678d9f6608374d75c60f165b85b6b444
EstEsc282 <estebanescudero282@gmail.com>
2021-03-10 01:44:03 -0500

    Création du poc barre de recherche #28

--- poc/recherche/recherche.js
+++ poc/recherche/recherche.js
@@ -1,41 +1,36 @@
 var httpRequest;
-  console.log("2");
-  document.getElementById("barre-de-recherche").addEventListener("change", makeRequest);
-  var url="https://www.promotemyjam.store/poc/recherche/listeItemAjax.php";
 
-  console.log("2");
+document.getElementById("barre-de-recherche").addEventListener("change", makeRequest);
+var url="https://www.promotemyjam.store/poc/recherche/listeItemAjax.php";
 
+function makeRequest() {
+  httpRequest = new XMLHttpRequest();
 
-  function makeRequest() {
-    httpRequest = new XMLHttpRequest();
+  if (!httpRequest) {
+    alert('Abandon :( Impossible de créer une instance de XMLHTTP');
+    return false;
+  }
+  console.log("1");
+  httpRequest.onreadystatechange = listeItem;
+  httpRequest.open('GET', url, true);
+  httpRequest.send();
+}
 
-    console.log("2");
-    if (!httpRequest) {
-      alert('Abandon :( Impossible de créer une instance de XMLHTTP');
-      return false;
+function listeItem(){
+  if (httpRequest.readyState === XMLHttpRequest.DONE) {
+    if (httpRequest.status === 200) {
+      alert(httpRequest.responseText);
+    } else {
+      alert('Il y a eu un problème avec la requête.');
     }
     console.log("1");
-    httpRequest.onreadystatechange = listeItem;
-    httpRequest.open('GET', url, true);
-    httpRequest.send();
+    for (var i = 0; i < document.getElementByTagName('nom').length; i++) {
+    document.getElementByTagName('nom').values('nom');
+    console.log("2");
   }
-
-  function listeItem(){
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        var response = JSON.parse(httpRequest.responseText);
-        alert(httpRequest.responseText);
-      } else {
-        alert('Il y a eu un problème avec la requête.');
-      }
-      console.log("1");
-      for (var i = 0; i < response.getElementByTagName('nom').length; i++) {
-      response.getElementByTagName('nom').values('nom');
-      console.log("2");
-    }
-    }
   }
+}
 
-  function afficherItemRechercher(){
+function afficherItemRechercher(){
 
-  }
+}
