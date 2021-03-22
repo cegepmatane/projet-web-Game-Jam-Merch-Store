002040a801ba85a6a7959029eb2b02edad553c1e
Hy-Vong <blobivon@yahoo.fr>
2021-03-22 19:53:36 -0400

    Fix affichage liste admin

--- admin/liste-admin.php
+++ admin/liste-admin.php
@@ -28,32 +28,26 @@ $listeItem = PromotemyjamDAO::listerItems();
     {
     ?>
 
-    <section class="section">
+    <section class="section-liste">
         <h1 class="titre"><?php echo $collection["nom"]; ?></h1>
-        <div class="contain">
-        
-        <section class="contenu-page">
-            <a href="ajouter-item.php">Ajouter un produit</a>
-            <a href="modifier-item.php">Modifier un produit</a>
+        <div class="contain-liste">
             <a href="supprimer-item.php">Supprimer un produit</a>
-
-        </section>
-            
-        <?php
-        foreach($listeItem as $item)
-        {
-            //$item = get_object_vars($item);
-            if($item["id_collection"] == $collection["id"])
+                
+            <?php
+            foreach($listeItem as $item)
             {
-        ?>
-                <div class="itemListe">
-                    <a href="modifier-item.php?id=<?php echo $item["id"]; ?>"><img src="../img/<?php echo $item['image']; ?>" width="150" height="150" alt="Item"></a>
-                    <h2 class="itemTitre" style="text-align: center;"><?= $item["nom"]; ?></h2>
-                </div>
-        <?php
+                //$item = get_object_vars($item);
+                if($item["id_collection"] == $collection["id"])
+                {
+            ?>
+                    <div class="itemListe">
+                        <a href="modifier-item.php?id=<?php echo $item["id"]; ?>"><img src="../img/<?php echo $item['image']; ?>" width="150" height="150" alt="Item"></a>
+                        <h2 class="itemTitre" style="text-align: center;"><?= $item["nom"]; ?></h2>
+                    </div>
+            <?php
+                }
             }
-        }
-        ?>
+            ?>
             <div id="bouton-ajouter">
                 <a href="ajouter-item.php?id_collection=<?php echo $collection['id']; ?>">+ Ajouter</a>
             </div>
