7133613c6cd74d93c89678ccd260463a211afa4f
EstEsc282 <estebanescudero282@gmail.com>
2021-05-28 16:35:58 -0400

    Mise à jour du poc barre de recherche #28

--- poc/recherche/recherche.js
+++ poc/recherche/recherche.js
@@ -60,7 +60,7 @@ function listeItem(){
 
       var recherche;
 
-      if(document.getElementById("barre-de-recherche").value !== null) {
+      if(x[i].getElementsByTagName("id")[0].childNodes[0].nodeValue >= 0) {
         recherche = recherchenotnull;
       }else{
         recherche = recherchenull;
