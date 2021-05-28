bce93c1c836073178d1fed279b3e2cbb1b5f9fc5
EstEsc282 <estebanescudero282@gmail.com>
2021-05-28 15:17:58 -0400

    Mise à jour du poc barre de recherche #28

--- poc/recherche/recherche.js
+++ poc/recherche/recherche.js
@@ -40,6 +40,7 @@ function listeItem(){
       //console.log(x[i].getElementsByTagName("nom")[0].childNodes[0].nodeValue);
       //console.log(x[i].getElementsByTagName("prix")[0].childNodes[0].nodeValue);
       document.getElementsByClassName("content").innerHTML =
+      '<div>' +
       '   <a href="item.php?id=' +
       x[i].getElementsByTagName("id")[0].childNodes[0].nodeValue +
       '"><img src=./img/item1.png></a><p>' +
@@ -47,7 +48,8 @@ function listeItem(){
       '</p>\n' +
       '   <span>' +
       x[i].getElementsByTagName("prix")[0].childNodes[0].nodeValue +
-      '$</span>';
+      '$</span>' +
+      '</div>';
     }
   } else {
         //alert('Il y a eu un problème avec la requête.');
